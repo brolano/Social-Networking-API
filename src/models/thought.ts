eb448194cd04b52899fd14a8c4d690ef0fe493db
Brian Solano <brian_solano110012@yahoo.com>
2025-02-07 23:52:11 -0600

    revised thought ts

--- src/models/thought.ts
+++ src/models/thought.ts
@@ -38,3 +38,69 @@ const reactionSchema = new Schema<IReaction>(
         id: false
     }
 );
+
+const thoughtSchema = new Schema<IThought>(
+    {
+        thoughtText: {
+            type: String,
+            required: [true, 'Thought text is required'],
+            minlength: 1,
+            maxlength: 280
+        },
+        username: {
+            type: String,
+            required: [true, 'Username is required']
+        },
+        reactions: [reactionSchema]
+    },
+    {
+        timestamps: true,
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+);
+
+thoughtSchema.virtual('reactionCount').get(function () {
+    return this.reactions.length;
+};)
+
+thoughtSchema.post('save', async function(doc) {
+    try {
+        const { User } = require('./user.model');
+        await User.findOneandUpdate(
+            { username: doc.username },
+            { $addToSet: { thoughts: doc._id } }
+        );
+    } catch (err) {
+        console.error('Error updating user thoughts:', err);
+    }
+});
+
+thoughtSchema.post('remove', async function(doc) {
+    try {
+        const { User } = require('.user.model');
+        await User.findOneAndUpdate(
+            { username: doc.username },
+            { $pull: { thoughts:doc._id } }
+        );
+    } catch (err) {
+        console.error('Error cleaning up user thoughts:', err);
+    }
+});
+
+thoughtSchema.methods.addReaction = async function(reactionData: Partial<IReaction>) {
+    this.reactions.push(reactionData);
+    return this.save();
+};
+
+thoughtSchema.methods.removeReaction = async function(reactionId: Types.ObjectId) {
+    this.reactions = this.reactions.filter(
+        reaction => !reaction.reactionId.equals(reactionId)
+    );
+    return this.save();
+};
+
+export const Thought = model<IThought>('Thought', thoughtSchema);
