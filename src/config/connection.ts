b59a0db45cbf8c50a907b05192943031c0ae3733
Brian Solano <brian_solano110012@yahoo.com>
2025-02-09 00:32:59 -0600

    updated connection ts

--- src/config/connection.ts
+++ src/config/connection.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const db = async (): Promise<typeof mongoose.connection> =>{
     try {
-        await mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/socialNetworkingDB');
+        await mongoose.connect(process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/socialNetworkingDB');
         console.log('Database connected.');
         return mongoose.connection;
     } catch(error) {
